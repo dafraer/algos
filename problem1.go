023b3cdaf399275d06bc78d08f2529f812650e1b
Kamil <kdnuriev@gmail.com>
2024-07-18 02:23:59 +0400

    problem 1

---
+++ problem1.go
@@ -0,0 +1,10 @@
+func twoSum(nums []int, target int) []int {
+    m := make(map[int]int)
+    for i := 0; i < len(nums);i++ {
+        if _, ok := m[target-nums[i]]; ok {
+            return []int{i, m[target-nums[i]]}
+        }
+        m[nums[i]] = i
+    }
+    return []int{}
+}
