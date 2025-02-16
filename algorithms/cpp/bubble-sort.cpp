93937a33dfdcb0945daaa4ab5ac9e071a051884f
Kamil <kdnuriev@gmail.com>
2025-02-16 19:25:51 +0400

    ps

--- algorithms/cpp/bubble-sort.cpp
+++ algorithms/cpp/bubble-sort.cpp
@@ -2,32 +2,31 @@
 
 using namespace std;
 
-
 vector<int> bubble_sort(vector<int> a) {
-    for (int i = 0; i < a.size(); i++) {
-        for (int j = 0; j < a.size()-1; j++) {
-            if (a[j] > a[j + 1]) {
-                swap(a[j], a[j+1]);
-            }
-        }
+  for (int i = 0; i < a.size(); i++) {
+    for (int j = 0; j < a.size() - 1; j++) {
+      if (a[j] > a[j + 1]) {
+        swap(a[j], a[j + 1]);
+      }
     }
-    return a;
+  }
+  return a;
 }
 
 int main() {
-    //input number of elements
-    int n;
-    cin >> n;
-    //declare vector of size n
-    vector<int> a(n);
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    
-    //sort using bubble sort
-    a = bubble_sort(a, n);
-    for (int i = 0; i < n; i++) {
-        cout << a[i] << " ";
-    }
-    cout << "\n";
-} 
+  // input number of elements
+  int n;
+  cin >> n;
+  // declare vector of size n
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+
+  // sort using bubble sort
+  a = bubble_sort(a);
+  for (int i = 0; i < n; i++) {
+    cout << a[i] << " ";
+  }
+  cout << "\n";
+}
