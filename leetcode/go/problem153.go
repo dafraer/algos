d2e429bd9516014305db79fe349cdce719a1c2bb
Kamil <kdnuriev@gmail.com>
2025-02-23 23:10:08 +0400

    leetcode probelm 153 Go

--- leetcode/go/problem153.go
+++ leetcode/go/problem153.go
@@ -1,4 +1,13 @@
 func findMin(nums []int) int {
-	sort.Ints(nums)
-	return nums[0]
+	l := 0
+	r := len(nums) - 1
+	for l < r {
+		m := (r + l) / 2
+		if nums[m] > nums[r] {
+			l = m + 1
+		} else {
+			r = m
+		}
+	}
+	return nums[l]
 }
