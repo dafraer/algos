dc6e3e047736d8aad9f926eb5c5aa1aa08f0d3a9
Kamil <kdnuriev@gmail.com>
2025-03-19 01:00:42 +0400

    leetcode problem 2629 Javascript

---
+++ leetcode/js/problem2629.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    
+    return function(x) {
+        if (functions.length === 0) {
+            return x;
+        }
+        let ans = functions[functions.length-1](x);
+        for (let i = functions.length-2; i >= 0; i--) {
+            ans = functions[i](ans);
+        }    
+        return ans;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
