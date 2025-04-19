4c4834af92836422989c1e9b194002cfc0de38b3
Kamil <kdnuriev@gmail.com>
2025-04-19 12:21:49 +0400

    leetcode problem 105 Go

---
+++ leetcode/go/problem105.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 || len(inorder) == 0 {
+		return nil
+	}
+	root := &TreeNode{Val: preorder[0]}
+	mid := len(inorder) - 1
+	for i := 0; i < len(inorder); i++ {
+		if inorder[i] == preorder[0] {
+			mid = i
+		}
+	}
+	root.Left = buildTree(preorder[1:mid+1], inorder[:mid])
+	root.Right = buildTree(preorder[mid+1:], inorder[mid+1:])
+	return root
+}
