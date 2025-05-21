a3bab19a8ad57990ade96a9a68a705df0ce208ee
Kamil <kdnuriev@gmail.com>
2025-05-21 20:45:53 +0400

    leetcode problem 222 C

---
+++ leetcode/c/problem222.c
@@ -0,0 +1,12 @@
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int countNodes(struct TreeNode* root) {
+    if (!root) return 0;
+    return countNodes(root->left)+countNodes(root->right)+1;
+}
