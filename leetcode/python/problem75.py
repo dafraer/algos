98a5fe2414ad26b9c4d074f46161d7177cade382
Kamil <kdnuriev@gmail.com>
2026-04-11 21:25:49 +0400

    leetcode problem 75 Python

--- leetcode/python/problem75.py
+++ leetcode/python/problem75.py
@@ -3,5 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        cnt0, cnt1 = 0, 0
+        for x in nums:
+            if x == 0:
+                cnt0 += 1
+            elif x == 1:
+                cnt1 += 1
+
+        for i in range(len(nums)):
+            if cnt0 > 0:
+                nums[i] = 0
+                cnt0 -= 1
+            elif cnt1 > 0:
+                nums[i] = 1
+                cnt1 -= 1
+            else:
+                nums[i] = 2
 
