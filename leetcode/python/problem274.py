029453e73aab4b2e199cc34fb6a4a6e98a4cc8c8
Kamil <kdnuriev@gmail.com>
2026-06-17 17:04:09 +0400

    leetcode problem 274 Python

---
+++ leetcode/python/problem274.py
@@ -0,0 +1,7 @@
+ class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        ans = 0
+        for i in range(len(citations)):
+            ans = max(ans, min(len(citations)-i, citations[i]))
+        return ans
